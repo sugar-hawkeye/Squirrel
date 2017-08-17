7768ed9651dbe933017616438ebd332cd45c251e
wim <wim@wim-iMac.local>
2017-08-17 17:40:35 +0800

    提交Squirrel projec

---
+++ Squirrel/Classes/RooScene.cpp
@@ -0,0 +1,9 @@
+//
+//  RooScene.cpp
+//  Squirrel
+//
+//  Created by wim on 2017/8/17.
+//
+//
+
+#include "RooScene.hpp"
---
+++ Squirrel/Classes/RooScene.hpp
@@ -0,0 +1,14 @@
+//
+//  RooScene.hpp
+//  Squirrel
+//
+//  Created by wim on 2017/8/17.
+//
+//
+
+#ifndef RooScene_hpp
+#define RooScene_hpp
+
+#include <stdio.h>
+
+#endif /* RooScene_hpp */
